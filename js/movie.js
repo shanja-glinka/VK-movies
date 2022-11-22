ea50227bbdc367e01dccad53a7fa5937d17f3986
iLoveGachimuchi <rul3iofu1s7@outlook.com>
2022-11-22 14:40:44 +0300

    fix vktoken

--- js/movie.js
+++ js/movie.js
@@ -1,7 +1,8 @@
 //oauth.vk.com/authorize?client_id=6861903&scope=photos,audio,video,docs,notes,pages,status,offers,questions,wall,groups,messages,email,notifications,stats,ads,offline,docs,pages,stats,notifications&response_type=token
-//"https://api.vk.com/method/wall.get?owner_id=-26750264&count=15&access_token=20a2b475f12e4c76779e48fbccc497e7826124077282709cd32bd968ec79c4d0e22655fbf27b9eba30653&v=5.52"
-//"https://api.vk.com/method/wall.get?owner_id=-26750264&count=20&access_token=27d6b6701f6d72a7e1c0b3bdf523f025fbf994cd6a3ffe1c9bf12e9424fd18b76deb390c85b07869af0bd&v=5.52"
-
+//"https://api.vk.com/method/wall.get?owner_id=-35785167&count=15&access_token=20a2b475f12e4c76779e48fbccc497e7826124077282709cd32bd968ec79c4d0e22655fbf27b9eba30653&v=5.52"
+//"https://api.vk.com/method/wall.get?owner_id=-35785167&count=20&access_token=27d6b6701f6d72a7e1c0b3bdf523f025fbf994cd6a3ffe1c9bf12e9424fd18b76deb390c85b07869af0bd&v=5.52"
+// Zui9dt6vScorWu1vNKXC
+// cc9e1971cc9e1971cc9e19715dccf6ad3eccc9ecc9e197190f846ed5dba1e268a3b4173
 const COMANDLINE_JS = new t_command;
 const cmd = function (c) {
   return COMANDLINE_JS.c(c);
@@ -26,8 +27,8 @@ var addMovieBlock = function (id, image, likes, title, info, desc, srcWatch) {
 
 var prepareUrl = function (vkAPImethod, id, count) {
   count = count || 1;
-  let accessToken = "access_token=" + "20a2b475f12e4c76779e48fbccc497e7826124077282709cd32bd968ec79c4d0e22655fbf27b9eba30653";
-  return "https://api.vk.com/method/" + vkAPImethod + "?owner_id=" + id + "&count=" + count + "&" + accessToken + "&v=5.52";
+  let accessToken = "access_token=" + "cc9e1971cc9e1971cc9e19715dccf6ad3eccc9ecc9e197190f846ed5dba1e268a3b4173";
+  return "https://api.vk.com/method/" + vkAPImethod + "?owner_id=" + id + "&count=" + count + "&" + accessToken + "&v=5.81";
 };
 
 var ajaxRequest = function (object) {
@@ -51,8 +52,9 @@ var vkAPIResponse = function (responseData) {
 };
 
 var parseResponse = function (jsonObj, condition) {
+  
   jsonObj = jsonObj.response.items;
-
+  
   var f1 = (v, s) => {
     v = v.split(s);
 
@@ -63,23 +65,31 @@ var parseResponse = function (jsonObj, condition) {
       }
     }
 
-
     if (v[0].length > 40) v = v.splice(0, 1);
     return v;
   };
 
   var f2 = (v, c) => {
-    v = v.split("#");
+    if (!v || typeof v == "undefined")
+      return "";
+    v = v.split(",");
 
-    if (v[0].indexOf("Жанр") > -1) v.splice(0, 1);
-
+    
+    if (v[0].indexOf("Жанр") > -1) v[0] = v[0].replace("Жанр", "").trim();
+    
     for (let i = 0; i < v.length; i++) {
-      if (v[i][0] === "#")
+      v[i] = v[i].trim();
+      if (v[i][0] === ":")
         v[i] = v[i].substr(1);
-      else {
-        v[i] = v[i].substr(0, v[i].indexOf("@"));
-      }
+      v[i] = v[i].trim();
     }
+    vcopy = [];
+
+    for (let i = 0; i < v.length; i++)
+      if (v[i].length > 0)
+          vcopy.push(v[i]);
+
+    v = vcopy;
 
     if (c && c.constructor === Array && c.length > 0) {
       let trigLength = c.length;
@@ -98,7 +108,10 @@ var parseResponse = function (jsonObj, condition) {
 
   var arrRet = [];
 
+  
   for (var i = 0; i < jsonObj.length; i++) {
+    if (jsonObj[i]['marked_as_ads'] == 1)
+      continue;
     try {
       let tag, img, like, title, info, desc, src;
       let r = jsonObj[i];
@@ -111,22 +124,24 @@ var parseResponse = function (jsonObj, condition) {
         src: ""
       }
 
-      if (r.text.indexOf("Страна:") > 0 && r.text.indexOf("Жанр:") > 0 && r.text.indexOf("Рейтинги:") > 0) {
+      // r.text.indexOf("Страна:") > 0 && 
+      if (r.text.indexOf("Жанр:") > 0 && r.text.indexOf("Рейтинг:") > 0) {
         let textArr = f1(r.text, "\n");
 
-        tag = f2(textArr[2], condition);
+        tag = f2((textArr[2].indexOf("Жанр") > -1 ? textArr[2]: textArr[3]), condition);
+        
         if (tag == "") continue;
 
-        img = r.attachments[0].photo.photo_807;
+        img = r.attachments[0].photo.sizes[4].url;
         like = r.likes.count;
         title = textArr[0].substr(0, textArr[0].indexOf("(")).trim();
 
-        info = textArr[0].substr(textArr[0].indexOf("(")).trim();
+        info = textArr[0].substr(textArr[0].indexOf("(") + 1, 4);
         info += " " + textArr[1].substr(textArr[1].indexOf(":") + 1).trim();
         info += ", " + tag;
 
-        desc = textArr[6].trim().substr(0, 185) + "...";
-        src = "https://vk.com/public26750264?w=wall" + r.owner_id + "_" + r.id;
+        desc = (textArr[4] ? textArr[4] : textArr[3]).trim().substr(0, 185) + "...";
+        src = "https://vk.com/public35785167?w=wall" + r.owner_id + "_" + r.id;
 
 
         obj.images = img;
@@ -139,7 +154,8 @@ var parseResponse = function (jsonObj, condition) {
         arrRet.push(obj);
       }
 
-    } catch {
+    } catch (ex) {
+      // console.log(ex);
       continue;
     }
   }
@@ -177,7 +193,7 @@ function setTagSelected(e) {
 function getVKMovies(e) {
   setTagSelected(e);
   ajaxRequest({
-    url: prepareUrl("wall.get", "-26750264", 100),
+    url: prepareUrl("wall.get", "-35785167", 100),
     method: "GET",
     success: vkAPIResponse
   });
@@ -211,6 +227,7 @@ function createContent(vkResponse) {
       "Нет фильмов соответствующим тэгам",
       ""
     );
+    document.getElementById("movie-card-0").style.backgroundSize = "80%";
     return;
   }
 
